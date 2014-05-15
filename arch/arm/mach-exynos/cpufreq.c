8a2dfbdd1aecf75e08a0b206b1fa4b6bf3a18df8
kgp700 <neokim20@gmail.com>
2014-05-16 01:06:08 +0900

    Change cpufreq transition latency

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -714,7 +714,7 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(exynos_info->freq_table, policy->cpu);
 
 	/* set the transition latency value */
-	policy->cpuinfo.transition_latency = 92000;
+	policy->cpuinfo.transition_latency = 45000;
 
 	/*
 	 * EXYNOS4 multi-core processors has 2 cores
